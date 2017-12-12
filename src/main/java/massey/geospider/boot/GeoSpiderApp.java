8e82a15b97992fbde342f84cc488fdb8f0ed446a
frank <frank@10.101.235.186>
2017-12-12 23:09:44 +1300

    Print banner for GeoSpider application in command line

--- src/main/java/massey/geospider/boot/GeoSpiderApp.java
+++ src/main/java/massey/geospider/boot/GeoSpiderApp.java
@@ -13,18 +13,40 @@ public class GeoSpiderApp {
 
     private static final Logger log = Logger.getLogger(GeoSpiderApp.class);
 
+    private static final String[] BANNER = {
+    "",
+    "/\\\\       ____                  ____            _       _               \\ \\ \\ \\",     
+    "(  )     / ___|   ___    ___   / ___|   _ __   (_)   __| |   ___   _ __   \\ \\ \\ \\",
+    " /\\\\    | |  _   / _ \\  / _ \\  \\___ \\  | '_ \\  | |  / _` |  / _ \\ | '__|    ) ) ) )",
+    "  '     | |_| | |  __/ | (_) |  ___) | | |_) | | | | (_| | |  __/ | |        ) ) ) )",
+    "  '      \\____|  \\___|  \\___/  |____/  | .__/  |_|  \\__,_|  \\___| |_|     / / / /",
+    "  '                                    |_|                               / / / /"};
+    
     public static void main(String[] args) {
-        log.info("========================================");
-        log.info("|| Geospider is going to work...      ||");
-        log.info("========================================");
+
+        printBanner();
 
         GeoCmdLine geoCmdLine = GeoCmdLineBuilder.build(args);
-        if (geoCmdLine != null)
+        if (geoCmdLine != null) {
+            log.info("========================================");
+            log.info("|| Geospider is going to work...      ||");
+            log.info("========================================");
+
             GeoController.getSingleton().dispatch(geoCmdLine);
 
-        log.info("================");
-        log.info("|| All done!  ||");
-        log.info("================");
+            log.info("================");
+            log.info("|| All done!  ||");
+            log.info("================");
+        }
     }
 
+    /**
+     * Prints the top banner
+     */
+    public static void printBanner() {
+        for (String line : BANNER) {
+            System.out.println(line);
+        }
+    }
+    
 }
